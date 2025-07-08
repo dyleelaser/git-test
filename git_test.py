8d3117519bea57441de93ed319b641a3616e1c81
Dal Yong Lee <dylee.physics@gmail.com>
2025-07-08 16:41:28 +0900

    Add: timestamp to hello() output

--- git_test.py
+++ git_test.py
@@ -1,11 +1,15 @@
+# imports
+from datetime import datetime
+
 # Here, define function
 def hello(name: str):
     if not isinstance(name, str):
         raise TypeError("name must be a string")
-    print(f"Hello, {name}!")
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Hello, {name}! It's [{now}]")
 
 # This is timetravel time.
 #print("Hello, Timekeeper.")
+#hello(123)
 hello("Timekeeper")
-hello(123)
 
