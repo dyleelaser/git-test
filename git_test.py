2c022021a5dafbcab1491644752ba614e99075aa
Dal Yong Lee <dylee.physics@gmail.com>
2025-07-08 16:24:05 +0900

    Fix hello() function.

--- git_test.py
+++ git_test.py
@@ -1,7 +1,11 @@
 # Here, define function
-def hello(name):
-    print("Hello, {name}!")
+def hello(name: str):
+    if not isinstance(name, str):
+        raise TypeError("name must be a string")
+    print(f"Hello, {name}!")
 
 # This is timetravel time.
 #print("Hello, Timekeeper.")
 hello("Timekeeper")
+hello(123)
+
