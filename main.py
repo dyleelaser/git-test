c1c5767b03e1bb776d5d9f0f61569ecbe9471c3c
Dal Yong Lee <dylee.physics@gmail.com>
2025-07-09 10:46:31 +0900

    Refactor unit test for hello function

--- main.py
+++
@@ -1,7 +0,0 @@
-# test_git_test.py
-from src.git_test import hello
-
-def test_hello_prints_correctly(capfd):
-    hello("Testuser")
-    out, err = capfd.readouterr()
-    assert "Hello, Testuser" in out
