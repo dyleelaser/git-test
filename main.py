4ace85822f45cc4ad132186a31d25800fab6a66f
Dal Yong Lee <dylee.physics@gmail.com>
2025-07-09 11:18:02 +0900

    Modify: basic main structure

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# main.py
+
 # imports
 from src.git_test import hello
 
+# main function
+def main():
+    name = "Timekeeper"
+    hello(name)
+
 # main
 if __name__ == "__main__":
-    hello("Timekeeper")
+    main()
