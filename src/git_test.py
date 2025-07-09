8535fe5942c8868651ce41f60c74822528808575
Dal Yong Lee <dylee.physics@gmail.com>
2025-07-09 17:54:24 +0900

    Feat: add magical emoji to greeting

--- src/git_test.py
+++ src/git_test.py
@@ -7,7 +7,7 @@ def hello(name: str):
     if not isinstance(name, str):
         raise TypeError("name must be a string")
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Hello, {name}! It's [{now}]")
+    print(f"🐧 Greetings, {name}! It's [{now}]")
 
 # This is timetravel time.
 #print("Hello, Timekeeper.")
